ddd3c501bd1a165203e64e9fb62788d668f0201e
ronakdudhani <ronakdudhani@gmail.com>
2024-08-08 13:50:15 +0530

    Completed NavBar

---
+++ blogging website - frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>sign in</h1>} />
+        <Route path="signup" element={<h1>sign up</h1>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
