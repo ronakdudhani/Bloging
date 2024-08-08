c2befc14e61e81e22504dbb01468127a6583987b
ronakdudhani <ronakdudhani@gmail.com>
2024-08-09 01:37:27 +0530

    Made SignIn and SignUp form

---
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -0,0 +1,26 @@
+import { AnimatePresence, motion } from "framer-motion";
+
+const AnimationWrapper = ({
+  children,
+  initial = { opacity: 0 },
+  animate = { opacity: 1 },
+  transition = { duration: 1 },
+  keyvalue,
+  className,
+}) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={keyvalue}
+        initial={initial}
+        animate={animate}
+        transition={transition}
+        className={className}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default AnimationWrapper;
